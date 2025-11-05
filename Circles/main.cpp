8e9fb357ba8331a56fb979889ca7b4713040f104
Hayden Thorpe <87242997+ThorpeDevs@users.noreply.github.com>
2025-11-05 16:30:43 +0000

    Added Circle Bouncing Off Sides

--- Circles/main.cpp
+++ Circles/main.cpp
@@ -20,22 +20,33 @@ int main()
 {
 	cout << "Hello circles" << endl;
 
-	int xDIR1 = 5;
-	int yDIR1 = 10;
+	bool xDIR1 = false;
+	bool yDIR1 = false;
+
+	int xLOC1 = 0;
+	int yLOC1 = 0;
+	int scale = 20;
+	int xspeed = 5;
+	int yspeed = 5;
 
 	while(UpdateFramework())
 	{
-		// Draws a circle at 100,200 with radius 20
-		DrawCircle(xDIR1,yDIR1,20);	
+		// Updates location based on direction variable
+		if (xDIR1 == true){xLOC1-= xspeed;}
+		else {xLOC1+= xspeed;}
 
-		if (xDIR1 == gScreenWidth)
-		{
-			xDIR1 = 0;
-		}
-		if (yDIR1 == gScreenHeight)
-		{
-			yDIR1 = 0;
-		}
+		if (yDIR1 == true){yLOC1-= yspeed;}
+		else { yLOC1+= yspeed; }
+
+		// Checks for boundaries and reverses direction variable
+		if (xLOC1 == gScreenWidth-(scale * 2)) { xDIR1 = true; }
+		else if (xLOC1 == 0) { xDIR1 = false; }
+
+		if (yLOC1 == gScreenHeight-(scale * 2)) { yDIR1 = true; }
+		else if (yLOC1 == 0) { yDIR1 = false; }
+
+		// Draws a circle at 100,200 with radius 20
+		DrawCircle(xLOC1,yLOC1,scale);
 	}
 
 	cout << "Bye bye" << endl;
